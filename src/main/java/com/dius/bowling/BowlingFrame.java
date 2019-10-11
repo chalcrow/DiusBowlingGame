dfc9c01bfd02ddfc9e48fd1869dbacb1e1526098
chalcrow <chris.halcrow123@gmail.com>
2019-10-11 22:15:36 +1100

    Added BowlingFrame class. Additional code for DiusBowlingGame

---
+++ src/main/java/com/dius/bowling/BowlingFrame.java
@@ -0,0 +1,12 @@
+package main.java.com.dius.bowling;
+
+public class BowlingFrame {
+
+    public int firstRollPins;
+    public int secondRollPins;
+    public boolean isStrike;
+    public boolean isSpare;
+    public int rollNumberInFrame;
+
+    public BowlingFrame() {}
+}
--- src/main/java/com/dius/bowling/DiusBowlingGame.java
+++ src/main/java/com/dius/bowling/DiusBowlingGame.java
@@ -3,23 +3,67 @@ import java.util.*;
 
 public class DiusBowlingGame implements BowlingGame {
 
+    final int pinsPerFrame = 10;
     public int totalScore = 0;
-    public int totalFramesPlayed = 0;
-    public boolean isStrike = false;
 
-    public int currentFrame = 0;
-    public int rollNumberWithinFrame = 0;
-    public int[][] frameScores = new int[10][2];
+    ArrayList<BowlingFrame> gameFrames = new ArrayList<BowlingFrame>();
 
     public void roll (int noOfPins) {
 
-        if(noOfPins == 10) {
-            isStrike = true;
-            currentFrame += 1;
+        if (getCurrentFrame().rollNumberInFrame == 1) {
+            // a strike can only occur on the 1st roll - we need to check if this is a strike
+            if (isStrike(noOfPins)) {
+                getCurrentFrame().isStrike = this.isStrike(noOfPins);
+                advanceFrame();
+            }
+        }
+        else {
+            // this is the 2nd roll
+            // a spare can only occur on the 2nd roll - we need to check if this is a spare
+            if(isSpare(noOfPins)) {
+
+            }
+            advanceFrame();
         }
 
+        advanceRollNumber();
         totalScore += noOfPins;
     }
+
+    private void advanceRollNumber() {
+        if ((getCurrentFrame().rollNumberInFrame == 1)) {
+            getCurrentFrame().rollNumberInFrame = 2;
+        } else {
+            advanceFrame();
+        }
+    }
+
+    private boolean isSpare(int noOfPins) {
+        return getCurrentFrame().firstRollPins + noOfPins == pinsPerFrame;
+    }
+
+    private boolean isStrike(int noOfPins) {
+        return noOfPins == pinsPerFrame;
+    }
+
+    public BowlingFrame getCurrentFrame() {
+        return this.gameFrames.get(this.gameFrames.size() - 1);
+    }
+
+    private void advanceFrame() {
+        if (gameFrames.size() < 10) {
+            BowlingFrame newFrame = new BowlingFrame();
+            newFrame.rollNumberInFrame = 1;
+            gameFrames.add(newFrame);
+        }
+        else completeGame();
+    }
+
+    private void completeGame() {
+        // TODO: do something to render the game complete
+    }
+
+
 
     public int score () {
 
